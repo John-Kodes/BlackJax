2e40ee8154142c66061b7fa900b2b40f47f2a0cd
John-Kodes <jdanielsemine@gmail.com>
2021-11-04 17:56:46 +0400

    leaderboard gets leaderboard with user rank data conditionally

--- src/pages/LeaderboardPage.js
+++ src/pages/LeaderboardPage.js
@@ -1,34 +1,52 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
+import Cookies from "js-cookie";
 // Components
 import BasePage from "../components/BasePage";
 import LeaderboardModal from "../components/LeaderboardModal";
+// Context
+import AuthContext from "../AuthContext";
 // Config
 import { API_URL } from "../config";
+// style
+import styled from "styled-components";
 
 // user data: rank, username, color, highScore, currentScore
 
 const LeaderboardPage = () => {
+  const [currentUserRank, setCurrentUserRank] = useState(null);
   const [ranks, setRanks] = useState([]);
   const [page, setPage] = useState(1);
   const [resultsLength, setResultsLength] = useState(0);
 
+  const { user } = useContext(AuthContext);
+
+  // console.log(user);
+
+  // TODO: button for skipping to user's current rank position
+
   const getLeaderboard = async () => {
-    try {
-      setRanks(false);
-      console.log("loading results...");
+    setRanks(false);
 
-      const res = await fetch(
-        `${API_URL}/users/leaderboardForGuest?page=${page}`
-      );
+    const token = Cookies.get("jwt");
 
-      const users = await res.json();
+    const res = await fetch(
+      `${API_URL}/users/leaderboard${!user ? "ForGuest" : ""}?page=${page}`,
+      user
+        ? {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        : {}
+    );
 
-      setRanks(users.data.leaderboard);
-      setResultsLength(users.results);
-      return;
-    } catch (err) {
-      console.log(err);
-    }
+    const dataRes = await res.json();
+    console.log(dataRes);
+
+    if (user) setCurrentUserRank(dataRes.data.userRank);
+
+    setRanks(dataRes.data.leaderboard);
+    setResultsLength(dataRes.results);
   };
 
   useEffect(() => {
@@ -38,14 +56,23 @@ const LeaderboardPage = () => {
 
   return (
     <BasePage useContainer={true}>
+      <TestBtn>jump to your rank</TestBtn>
       <LeaderboardModal
         ranks={ranks}
         setPage={setPage}
         page={page}
         resultsLength={resultsLength}
+        currentUserRank={currentUserRank}
       />
     </BasePage>
   );
 };
+
+const TestBtn = styled.button`
+  position: fixed;
+  top: 2rem;
+  right: 2rem;
+  z-index: 1000;
+`;
 
 export default LeaderboardPage;
