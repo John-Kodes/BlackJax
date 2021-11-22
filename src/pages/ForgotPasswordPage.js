4fe1dd34f83df0b2875b371d8307515a9dc3199f
John-Kodes <jdanielsemine@gmail.com>
2021-11-22 17:33:44 +0400

    ErrorModal refactored

--- src/pages/ForgotPasswordPage.js
+++ src/pages/ForgotPasswordPage.js
@@ -1,39 +1,17 @@
-import React, { useContext } from "react";
+import React from "react";
 // Components
 import BasePage from "../components/BasePage";
 import ForgotPasswordModal from "../components/ForgotPasswordModal";
-// Context
-import AuthContext from "../AuthContext";
-// styles
-import styled from "styled-components";
+import ErrorModal from "../components/ErrorModal";
 
 const ForgotPasswordPage = () => {
-  const { error } = useContext(AuthContext);
-
   return (
     <BasePage>
-      {error && (
-        <ErrorContainer>
-          <p>{error}</p>
-        </ErrorContainer>
-      )}
+      <ErrorModal />
+
       <ForgotPasswordModal />
     </BasePage>
   );
 };
-
-const ErrorContainer = styled.div`
-  padding: 3rem 4rem;
-  width: 100%;
-  max-width: 46rem;
-
-  background-color: rgba(54, 7, 7, 0.39);
-  border: 1px solid #ff0040;
-  border-radius: 4px;
-
-  p {
-    font-size: 1.6rem;
-  }
-`;
 
 export default ForgotPasswordPage;
--- src/pages/ResetPasswordPage.js
+++ src/pages/ResetPasswordPage.js
@@ -1,39 +1,16 @@
-import React, { useContext } from "react";
+import React from "react";
 // Components
 import BasePage from "../components/BasePage";
 import ResetPasswordModal from "../components/ResetPasswordModal";
-// Context
-import AuthContext from "../AuthContext";
-// styles
-import styled from "styled-components";
+import ErrorModal from "../components/ErrorModal";
 
 const ResetPasswordPage = () => {
-  const { error } = useContext(AuthContext);
-
   return (
     <BasePage useContainer={true}>
-      {error && (
-        <ErrorContainer>
-          <p>{error}</p>
-        </ErrorContainer>
-      )}
+      <ErrorModal />
       <ResetPasswordModal />
     </BasePage>
   );
 };
-
-const ErrorContainer = styled.div`
-  padding: 3rem 4rem;
-  width: 100%;
-  max-width: 46rem;
-
-  background-color: rgba(54, 7, 7, 0.39);
-  border: 1px solid #ff0040;
-  border-radius: 4px;
-
-  p {
-    font-size: 1.6rem;
-  }
-`;
 
 export default ResetPasswordPage;
